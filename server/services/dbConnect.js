56e7ac2dd31e3bbf5d7b773d50aa7fb93745d134
DESKTOP-FEF5930\TriNghia-PC01 <nguyentd@tringhiatech.vn>
2018-09-28 14:05:35 +0700

    fix dbUri deploy

--- server/services/dbConnect.js
+++ server/services/dbConnect.js
@@ -2,7 +2,8 @@ var mongoose = require('mongoose');
 
 exports.connectMongoDb = function() {
     //connect to MongoDB
-    var mongodbUri = process.env.dbUriLocal ||  process.env.dbMlab
+    // var mongodbUri = process.env.dbUriLocal ||  process.env.dbMlab
+    var mongodbUri = "mongodb://nguyennd9zzz:ducnguyen1501@ds259912.mlab.com:59912/spy_amazon"
     const configs = {
         useNewUrlParser: true,
     };
